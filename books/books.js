5472c733cfdfaf30c2ee20b2bdd4709c91ccc98c
crossaaron <atcbcrossx@gmail.com>
2019-03-18 16:30:40 -0700

    added MLabconnection

--- books/books.js
+++ books/books.js
@@ -1,6 +1,18 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const PORT = 1234;
+
+const mongoose = require('mongoose');
+const MLAB_KEY = process.env.MLAB_KEY;
+
+
+mongoose.connect("mongodb+srv://aaroncross:" + MLAB_KEY + "@testcluster-zbqkz.mongodb.net/test?retryWrites=true", () => {
+    console.log('database connected!')
+    console.log('**************');
+
+});
 
 app.get('/', (req, res) => {
     console.log(res);
