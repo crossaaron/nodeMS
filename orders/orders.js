3e0c34d1e620d6c4e81c9a97be201dccc577b5f6
crossaaron <atcbcrossx@gmail.com>
2019-03-24 15:45:41 -0700

    added basic structure for order service

---
+++ orders/orders.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 1234;
+
+
+//customerID
+//bookID
+//gotDate
+//deliveryDate
+
+
+app.listen(PORT, () => {
+    console.log('Order Service Connected!')
+});
